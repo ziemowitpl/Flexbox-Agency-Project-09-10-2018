88c8985ef227e3d9822bd502ce230a2892fa1401
ziemowitpl <roninrallegro@gmail.com>
2018-10-10 15:56:04 +0200

    We create a jQuery function that when we click onto video__switch-container it pause the video and we click again it play video - this is done by checking if such a class exists

--- js/app.js
+++ js/app.js
@@ -4,6 +4,19 @@ $(document).ready(function () {
     $('.navBtn').click(function(){
         $('.nav').toggleClass('nav--show');
     })
+    // video switch
+    $('.video__switch-container').click(function(){
+        const value = $('.video__switch-btn').hasClass('btnSlide')
+        if (value){
+            $('.video__switch-btn').removeClass('btnSlide')
+            $('#video').get(0).play()
+            
+        }
+        else{
+            $('.video__switch-btn').addClass('btnSlide')
+            $('#video').get(0).pause()
+        }
+    })
 })
    
    
